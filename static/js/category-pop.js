1a4fc7fde0ea5c51efa652faf4a1a64d1812f648
hdlee0619 <dlgusehd1211@naver.com>
2023-01-16 22:05:44 +0900

    Chore: app.py 수정

--- static/js/category-pop.js
+++ static/js/category-pop.js
@@ -1,13 +1,13 @@
 // 카테고리 post 존재시 사라지게 하는 함수
-$(document).ready(function () {});
-let get_list;
-if (!get_list) {
-    $('.category').show();
-    $('.main').hide();
-} else {
-    $('.category').hide();
-    $('.main').show();
-}
+// $(document).ready(function () {});
+// let get_list;
+// if (!get_list) {
+//     $('.category').show();
+//     $('.main').hide();
+// } else {
+//     $('.category').hide();
+//     $('.main').show();
+// }
 
 // function checking() {
 //     $.ajax({
@@ -26,3 +26,17 @@ if (!get_list) {
 //         },
 //     });
 // }
+
+function category() {
+    let img_url = $('#category-img').val();
+    let category_name = $('#category-title').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/category',
+        data: { category_name: category_name, category_img_url: img_url },
+        success: function (response) {
+            alert(response['msg']);
+        },
+    });
+}
