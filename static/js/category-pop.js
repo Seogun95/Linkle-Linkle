7800a42e6b7eeea0a763410987aef19b2ed2b899
hdlee0619 <dlgusehd1211@naver.com>
2023-01-16 22:22:50 +0900

    feat: category 포스팅 기능 추가

--- static/js/category-pop.js
+++ static/js/category-pop.js
@@ -9,34 +9,46 @@
 //     $('.main').show();
 // }
 
-// function checking() {
-//     $.ajax({
-//         type: 'GET',
-//         url: '/post',
-//         data: {},
-//         success: function (response) {
-//             let check = response;
-//             if (check === null) {
-//                 $('.category').show();
-//                 $('.main').hide();
-//             } else {
-//                 $('.category').hide();
-//                 $('.main').show();
-//             }
-//         },
-//     });
-// }
+function check() {
+    $.ajax({
+        type: 'GET',
+        url: '/categories',
+        data: {},
+        success: function (response) {
+            let check = response;
+            if (!check) {
+                $('.category').show();
+                $('.main').hide();
+            } else {
+                $('.category').hide();
+                $('.main').show();
+            }
+        },
+    });
+}
 
+// 테스트 완료
 function category() {
-    let img_url = $('#category-img').val();
-    let category_name = $('#category-title').val();
+    let img_url = $('#categoryPop__url').val();
+    let category_name = $('#categoryPop__title').val();
 
     $.ajax({
         type: 'POST',
         url: '/category',
         data: { category_name: category_name, category_img_url: img_url },
         success: function (response) {
-            alert(response['msg']);
+            alert(response);
         },
     });
 }
+
+// function categoryPosting() {
+//     $.ajax({
+//         type: 'GET',
+//         url: '/categories',
+//         data: {},
+//         success: function (response) {
+//             let rows
+//         },
+//     });
+// }
