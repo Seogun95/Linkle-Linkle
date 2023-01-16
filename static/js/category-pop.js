097f034c9983e4044afffd5629d0064e417fcab8
hdlee0619 <dlgusehd1211@naver.com>
2023-01-16 23:23:50 +0900

    feat: 에러이미지 기능 추가, 포스팅 기능 추가
    사용자가 에러이미지 URL을 넣지 않았을 때, 대체 이미지 기능 추가
    카테고리 포스팅 기능 추가

--- static/js/category-pop.js
+++ static/js/category-pop.js
@@ -1,5 +1,8 @@
+$(document).ready(function () {
+    categoryPosting();
+});
+
 // 카테고리 post 존재시 사라지게 하는 함수
-// $(document).ready(function () {});
 // let get_list;
 // if (!get_list) {
 //     $('.category').show();
@@ -9,6 +12,7 @@
 //     $('.main').show();
 // }
 
+// ================================= issue
 function check() {
     $.ajax({
         type: 'GET',
@@ -26,8 +30,9 @@ function check() {
         },
     });
 }
+// =================================
 
-// 테스트 완료
+// ================================= 테스트 완료
 function category() {
     let img_url = $('#categoryPop__url').val();
     let category_name = $('#categoryPop__title').val();
@@ -42,13 +47,35 @@ function category() {
     });
 }
 
-// function categoryPosting() {
-//     $.ajax({
-//         type: 'GET',
-//         url: '/categories',
-//         data: {},
-//         success: function (response) {
-//             let rows
-//         },
-//     });
-// }
+// =================================
+
+// ================================= 테스트 완료
+function categoryPosting() {
+    $.ajax({
+        type: 'GET',
+        url: '/categories',
+        data: {},
+        success: function (response) {
+            let rows = response['categories'];
+            for (let i = 0; i < rows.length; i++) {
+                let category_title = rows[i]['name'];
+                let category_img = rows[i]['image'];
+                console.log(rows);
+                console.log(category_title);
+                let temp_html = `<div class="col cards-box" data-aos="fade-up" data-aos-delay="200" data-aos-easing="ease-in-out" data-aos-once="false">
+                                    <div class="cards-box__container logo">
+                                        <div class="cards-box__card" style="width: 18rem">
+                                            <a href="https://www.youtube.com/watch?v=GmxPT-P331s&t=1s">
+                                                <img src="${category_img}" class="cards-box__img" alt="bookimage" onerror="this.src='../static/img/error_img.png';" />
+
+                                                <p class="cards-box__body-title text-center">${category_title}</p>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>`;
+                $('#cards-box').append(temp_html);
+            }
+        },
+    });
+}
+// =================================
