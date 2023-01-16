76e265381c41de4271670fddac134be8340a42bf
dae-wook <wkdrn8860@naver.com>
2023-01-17 06:04:52 +0900

    fix: 2개씩 나오는거 수

--- static/js/posting-pop.js
+++ static/js/posting-pop.js
@@ -69,43 +69,43 @@ function urlPosting() {
     });
 }
 
-function postCatagory() {
-    let url_href = window.location.href;
-    let url = new URL(url_href);
-    let a = url.searchParams.get('id');
-    $.ajax({
-        type: 'GET',
-        url: `/api/posts?category_id=${a}`,
-        data: {},
-        success: function (response) {
-            let rows = response['posts'];
-            console.log(rows);
-            for (let i = 0; i < rows.length; i++) {
-                let url = rows[i]['link_url'];
-                let img = rows[i]['image'];
-                let title = rows[i]['title'];
-                let comments = rows[i]['desc'];
-                let user_nickname = rows[i]['author'];
-                let status = rows[i]['status'];
-                if (status === 0) {
-                    let temp_html = `
-                    <div class="col cards-box" data-aos="fade-up" data-aos-delay="200" data-aos-easing="ease-in-out" data-aos-once="false">
-                        <div class="cards-box__container logo">
-                            <div class="cards-box__card" style="width: 18rem">
-                                <a href="${url}">
-                                    <img src="${img}" class="cards-box__img" alt="bookimage" />
-
-                                    <p class="cards-box__body-title">${title}</p>
-                                </a>
-                                <p class="cards-box__body-comment">${comments}</p>
-                                <p class="cards-box__body-user">by. ${user_nickname}</p>
-                            </div>
-                        </div>
-                    </div>
-           `;
-                    $('#cards-box').append(temp_html);
-                }
-            }
-        },
-    });
-}
+// // function postCatagory() {
+// //     let url_href = window.location.href;
+// //     let url = new URL(url_href);
+// //     let a = url.searchParams.get('id');
+// //     $.ajax({
+// //         type: 'GET',
+// //         url: `/api/posts?category_id=${a}`,
+// //         data: {},
+// //         success: function (response) {
+// //             let rows = response['posts'];
+// //             console.log(rows);
+// //             for (let i = 0; i < rows.length; i++) {
+// //                 let url = rows[i]['link_url'];
+// //                 let img = rows[i]['image'];
+// //                 let title = rows[i]['title'];
+// //                 let comments = rows[i]['desc'];
+// //                 let user_nickname = rows[i]['author'];
+// //                 let status = rows[i]['status'];
+// //                 if (status === 0) {
+// //                     let temp_html = `
+// //                     <div class="col cards-box" data-aos="fade-up" data-aos-delay="200" data-aos-easing="ease-in-out" data-aos-once="false">
+// //                         <div class="cards-box__container logo">
+// //                             <div class="cards-box__card" style="width: 18rem">
+// //                                 <a href="${url}">
+// //                                     <img src="${img}" class="cards-box__img" alt="bookimage" />
+// //
+// //                                     <p class="cards-box__body-title">${title}</p>
+// //                                 </a>
+// //                                 <p class="cards-box__body-comment">${comments}</p>
+// //                                 <p class="cards-box__body-user">by. ${user_nickname}</p>
+// //                             </div>
+// //                         </div>
+// //                     </div>
+// //            `;
+// //                     $('#cards-box').append(temp_html);
+// //                 }
+// //             }
+// //         },
+// //     });
+// }
